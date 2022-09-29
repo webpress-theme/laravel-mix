8f4399a52f8b0f1868f1507010191ffa276d4112
thienbdvmms <108317166+thienbdvmms@users.noreply.github.com>
2022-09-29 09:27:52 +0700

    config

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,7 +12,6 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js');
-mix.extract();
 mix.sass('resources/sass/global.scss', 'public/css');
 mix.sass('resources/sass/vendor.scss', 'public/css');
 mix.sass('resources/sass/header.scss', 'public/css');
